950bae8a916372c5622ea6760d2dfd3e89988401
jtapiat <jtapiat@tapiat-17.10>
2018-07-31 13:05:39 +0200

    fly:  - First commit

---
+++ src/main/java/com/flyteam/mastermind/client/ApiClient.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flyteam.mastermind.client;
+
+import com.sun.jersey.api.client.Client;
+
+/**
+ *
+ * @author jtapiat
+ */
+public class ApiClient {
+
+}
