6a4912c710f30ea4777d9b77267f6cdeae9a4ec6
anasfalah <afalah-nyg@ymagis.com>
2018-08-01 16:26:44 +0100

    code inutile

--- src/main/java/com/flyteam/mastermind/App.java
+++ src/main/java/com/flyteam/mastermind/App.java
@@ -18,44 +18,6 @@ import com.flyteam.mastermind.client.ProposalResult;
  */
 public class App {
 
-    static Set<String> permutations;
-    static Set<String> result = new HashSet<String>();
-
-    public static Set<String> permutation(String string) {
-        permutations = new HashSet<String>();
-
-        int n = string.length();
-        for (int i = n - 1; i >= 0; i--) {
-            shuffle(string.charAt(i));
-        }
-        return permutations;
-    }
-
-    private static void shuffle(char c) {
-        if (permutations.size() == 0) {
-            permutations.add(String.valueOf(c));
-        } else {
-            Iterator<String> it = permutations.iterator();
-            for (int i = 0; i < permutations.size(); i++) {
-
-                String temp1;
-                for (; it.hasNext();) {
-                    temp1 = it.next();
-                    for (int k = 0; k < temp1.length() + 1; k += 1) {
-                        StringBuilder sb = new StringBuilder(temp1);
-
-                        sb.insert(k, c);
-
-                        result.add(sb.toString());
-                    }
-                }
-            }
-            permutations = result;
-            //'result' has to be refreshed so that in next run it doesn't contain stale values.
-            result = new HashSet<String>();
-        }
-    }
-
     /**
      *
      * @param args
@@ -72,7 +34,6 @@ public class App {
         ProposalResult proposalResult = apiClient.test(chInitiale);
         // archived first result
         firstOk = proposalResult.getGood();
-        int nb = 0;
         while (proposalResult.getGood() != size) {
             if (proposalResult.getGood() == size) {
                 // case when all is OK
@@ -81,7 +42,6 @@ public class App {
             } else {
                 for (int position = 0; position < size; position++) {
                     for (int i = position + 1; i < size; i++) {
-                        nb++;
                         String chReplaced = invertChar(chInitiale, position, i);
                         proposalResult = apiClient.test(chReplaced);
                         if (proposalResult.getGood() == size) {
